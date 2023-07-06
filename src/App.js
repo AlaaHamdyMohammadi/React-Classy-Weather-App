98621414a06de368c21b337d56c8148250f0eb8e
AlaaHamdyMohammadi <alaahamdy2197@gmail.com>
2023-07-06 19:50:37 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,14 +3,33 @@ import React from "react";
 class Counter extends React.Component{
   constructor(props){
     super(props)
-    this.state = {count: 5}
+    this.state = {count: 5};
+    this.handleDecrement = this.handleDecrement.bind(this);
+    //this.handleIncrement = this.handleIncrement.bind(this);
   }
+
+  handleDecrement(){
+    this.setState(curState => {
+      return {count: curState.count - 1};
+    })
+  };
+  handleIncrement = () => {
+    this.setState(curState => {
+      return {count: curState.count + 1};
+    })
+  };
+
   render(){
-    return <div>
-      <button>-</button>
-      <span>{this.state.count}</span>
-      <button>+</button>
-    </div>
+
+    const date = new Date('feb 20 2025');
+    date.setDate(date.getDate() + this.state.count);
+    return (
+      <div>
+        <button onClick={this.handleDecrement}>-</button>
+        <span>{date.toDateString()} [{this.state.count}]</span>
+        <button onClick={this.handleIncrement}>+</button>
+      </div>
+    );
   }
 };
 export default Counter;
